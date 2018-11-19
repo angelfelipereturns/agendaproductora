58a1a3ecd81e4ee1f16d58edf8c49a1661bad280
afaf <faded662@gmail.com>
2018-11-19 04:27:27 +0100

    test application

--- src/test/java/com/afaf/agendaproductora/AgendaProductoraApplicationTests.java
+++ src/test/java/com/afaf/agendaproductora/AgendaProductoraApplicationTests.java
@@ -1,16 +1,31 @@
 package com.afaf.agendaproductora;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import com.afaf.agendaproductora.rest.ContactoRestController;
+import com.afaf.agendaproductora.service.ContactoService;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class AgendaProductoraApplicationTests {
+	
+	@Autowired
+	private ContactoRestController contactoRestController;
+	
+	@Autowired
+	private ContactoService contactoService;
 
+	
 	@Test
 	public void contextLoads() {
+		assertThat(contactoRestController).isNotNull();
+		assertThat(contactoService).isNotNull();
 	}
 
 }
